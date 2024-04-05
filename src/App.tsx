d121a4c5fff549dc7ff0ea97f284fff7b4d90c58
Mojidul Islam <mojidul.dev.pwd@gmial.com>
2024-04-05 12:02:15 +0600

    add-loading

--- src/App.tsx
+++ src/App.tsx
@@ -37,11 +37,14 @@ import User from './components/User';
 
 const App: FC<AppProps> = ({ title }) => {
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const [users, setUsers] = useState<Users[]>([]);
 
   useEffect(() => {
     const getUsers = async () => {
       try {
+        setIsLoading(true);
         const { data } = await axios.get(
           'https://randomuser.me/api/?results=10'
         );
@@ -49,6 +52,8 @@ const App: FC<AppProps> = ({ title }) => {
         setUsers(data.results);
       } catch (error) {
         console.log(error);
+      }finally {
+        setIsLoading(false);
       }
     };
     getUsers();
@@ -57,6 +62,7 @@ const App: FC<AppProps> = ({ title }) => {
   return (
     <div>
       <h1>{title}</h1>
+      {isLoading && <p>Loading...</p>}
       <ul>
         {/* {users.map(({ login, name, email }) => {
           return (
